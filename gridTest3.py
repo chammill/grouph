4d2a8055234c795512e2e45b5c3ccf2d047c0155
Jeffrey Rice <jeff@eventup.com>
2018-04-25 12:03:19 -0500

    updated top down
    connects to puzzle and levels

--- gridTest3.py
+++ gridTest3.py
@@ -9,7 +9,7 @@ import pygame.event as EVENTS
 winWidth = 800
 winHeight = 600
 FPS = 60
-
+level = 1
 
 # variables for commonly used colours
 BLACK = (0, 0, 0)
@@ -20,6 +20,52 @@ MAGENTA = (255, 0, 255)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
+
+
+
+x = 30
+y = 40
+width = 20
+height = 20
+vel = 5
+block_size = 10
+
+lineX = 30
+lineY = 46
+lineWidth = 500
+lineHeight = 5
+
+RED = (255,0,0)
+GREEN = (0, 255,0)
+
+toggle1 = RED
+toggle2 = RED
+toggle3 = RED
+toggle4 = RED
+
+global lever1
+lever1 = False
+global lever2
+lever2 = False
+global lever3
+lever3 = False
+global lever4
+lever4 = False
+
+
+leverWidth = 20
+leverHeight = 30
+leverY = 500
+
+lever1X = 120
+lever2X = 220
+lever3X = 320
+lever4X = 420
+
+cirRad = 10
+
+
+
 
 # initialise pygame settings and create game window
 interaction = "empty"
@@ -47,7 +93,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         # load ship image & scale to fit game window...
-        self.image = pygame.transform.scale(char_img, (49, 37))
+        self.image = pygame.transform.scale(char_img, (24, 30))
         # set colorkey to remove white background for char's rect
         self.image.set_colorkey(WHITE)
         #check images and get rect...
@@ -147,35 +193,35 @@ wall_list = pygame.sprite.Group()
 
 
 # left wall 
-wall = Wall(0, 0, 25, 600)
+wall = Wall(0, 110, 25, 600)
 wall_list.add(wall)
 
 # top left wall 
-wall = Wall(0, 0, 370, 60)
+wall = Wall(0, 110, 370, 60)
 wall_list.add(wall)
 
 # top right wall 
-wall = Wall(460, 0, 370, 60)
+wall = Wall(460, 110, 370, 60)
 wall_list.add(wall)
 
 # right wall 
-wall = Wall(775, 10, 25, 600)
+wall = Wall(775, 130, 25, 600)
 wall_list.add(wall)
 
 # top right desk 
-wall = Wall(470, 145, 60, 40)
+wall = Wall(470, 265, 60, 40)
 wall_list.add(wall)
 
 # top left desk 
-wall = Wall(290, 145, 60, 40)
+wall = Wall(290, 265, 60, 40)
 wall_list.add(wall)
 
 # bottom right desk 
-wall = Wall(470, 255, 60, 50)
+wall = Wall(470, 375, 60, 50)
 wall_list.add(wall)
 
 # bottom left desk 
-wall = Wall(290, 255, 60, 50)
+wall = Wall(290, 375, 60, 50)
 wall_list.add(wall)
 
 
@@ -191,19 +237,34 @@ class Bookshelf(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         # load ship image & scale to fit game window...
-        self.image = bookshelf_img
+        self.image = pygame.transform.scale(bookshelf_img, (24, 30))
         # set colorkey to remove black background for ship's rect
         self.image.set_colorkey(GREEN)
         #check images and get rect...
         self.rect = self.image.get_rect()
         # set radius for circle bounding
-        self.radius = 50
+        self.radius = 2
         pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.centerx = winWidth / 3
         self.rect.bottom = winHeight - 20
 
 
 
+# create a scientist
+class Scientist(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        # load ship image & scale to fit game window...
+        self.image = scientist_img
+        # set colorkey to remove black background for ship's rect
+        self.image.set_colorkey(WHITE)
+        #check images and get rect...
+        self.rect = self.image.get_rect()
+        # set radius for circle bounding
+        self.radius = 1
+        pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.centerx = winWidth - 300
+        self.rect.bottom = winHeight - 280
 
 
 
@@ -215,20 +276,29 @@ def gameExit():
     sys.exit()
 
 # load graphics/images for the game
-bg_img = pygame.image.load(os.path.join(img_dir, "main_room.png")).convert()
+bg_img = pygame.image.load(os.path.join(img_dir, "elevatorFinal.png")).convert()
 # add rect for bg - helps locate background
 bg_rect = bg_img.get_rect()
+
 # load graphics/images for the game
-bg_img2 = pygame.image.load(os.path.join(img_dir, "main_room2.png")).convert()
+bg_img2 = pygame.image.load(os.path.join(img_dir, "officeLevelNew.png")).convert()
 # add rect for bg - helps locate background
 bg_rect2 = bg_img2.get_rect()
+
+# load graphics/images for the game
+bg_lvl3 = pygame.image.load(os.path.join(img_dir, "officeLevelNew.png")).convert()
+# add rect for bg - helps locate background
+bg_rect3 = bg_img2.get_rect()
+
+
 # player's character
 char_img = pygame.image.load(os.path.join(img_dir, "char1.png")).convert()
 # bookshelf
 bookshelf_img = pygame.image.load(os.path.join(img_dir, "bookshelf-green.png")).convert()
 #security camera spotlights
 security_spot = pygame.image.load(os.path.join(img_dir, "camera_spot.png")).convert()
-
+#scientist
+scientist_img = pygame.image.load(os.path.join(img_dir, "scientist.png")).convert()
 
 
 
@@ -284,15 +354,15 @@ class Security(pygame.sprite.Sprite):
 
 
 
-security_spot1 = Security(120, 140)
-security_spot2 = Security(365, 140)
-security_spot3 = Security(120, 240)
-security_spot4 = Security(120, 380)
+security_spot1 = Security(120, 260)
+security_spot2 = Security(365, 260)
+security_spot3 = Security(120, 360)
+security_spot4 = Security(120, 500)
 
-security_spot5 = Security(620, 140)
+security_spot5 = Security(620, 260)
 
-security_spot6 = Security(465, 380)
-security_spot7 = Security(665, 380)
+security_spot6 = Security(465, 500)
+security_spot7 = Security(665, 500)
 
 
 # sprite groups - game, mob, projectiles...
@@ -310,11 +380,12 @@ all_sprite_list.add(player)
 
 
 bookshelf = Bookshelf()
+scientist = Scientist()
 
 security_spots1.add(security_spot1, security_spot2, security_spot3, security_spot4, security_spot5, security_spot6, security_spot7)
 
 # add sprite to game's sprite group
-game_sprites.add(player, bookshelf, security_spots1)
+game_sprites.add(player, bookshelf, security_spots1, scientist)
 # add sprites for security spots
 
 # new bookshelf group
@@ -326,24 +397,210 @@ def interAction():
     if interaction == "bookshelf":
         nextLvl()
         
+def gameExit():
+    pygame.quit()
+    sys.exit()
 
+def mouseClick():
+    coor = pygame.mouse.get_pos()
+    global lever1
+    global lever2
+    global lever3
+    global lever4
+
+    global toggle1
+    global toggle2
+    global toggle3
+    global toggle4
+    if (lever1X<coor[0]<(lever1X+leverWidth)):
+        if (leverY<coor[1]<(leverY+leverHeight)):
+            if (lever1 == False):
+                lever1 = True
+                toggle1 = GREEN
+            else:
+                lever1 = False
+                toggle1 = RED
+            if (lever3 == False):
+                lever3 = True
+                toggle3 = GREEN
+            else:
+                lever3 = False
+                toggle3 = RED
+    if (lever2X < coor[0] < (lever2X + leverWidth)):
+        if (leverY < coor[1] < (leverY + leverHeight)):
+            if (lever1 == False):
+                lever1 = True
+                toggle1 = GREEN
+            else:
+                lever1 = False
+                toggle1 = RED
+            if (lever4 == False):
+                lever4 = True
+                toggle4 = GREEN
+            else:
+                lever4 = False
+                toggle4 = RED
+    if (lever3X < coor[0] < (lever3X + leverWidth)):
+        if (leverY < coor[1] < (leverY + leverHeight)):
+            if (lever2 == False):
+                lever2 = True
+                toggle2 = GREEN
+            else:
+                lever2 = False
+                toggle2 = RED
+            if (lever3 == False):
+                lever3 = True
+                toggle3 = GREEN
+            else:
+                lever3 = False
+                toggle3 = RED
+    if (lever4X < coor[0] < (lever4X + leverWidth)):
+        if (leverY < coor[1] < (leverY + leverHeight)):
+            if (lever2 == False):
+                lever2 = True
+                toggle2 = GREEN
+            else:
+                lever2 = False
+                toggle2 = RED
+            if (lever4 == False):
+                lever4 = True
+                toggle4 = GREEN
+            else:
+                lever4 = False
+                toggle4 = RED
+
+## Puzzle Level 1
 def nextLvl():
     
 
-
-
-
+    
+    ## Remove the collision walls
     wall_list.empty()
     game_sprites.remove(security_spots1)
 
 
 
+    pygame.mixer.init()
+
+    ## Reset the collision walls for the next level
+
+    # left wall 
+    wall = Wall(0, 110, 25, 600)
+    wall_list.add(wall)
+
+    # top left wall 
+    wall = Wall(0, 110, 370, 60)
+    wall_list.add(wall)
+
+    # top right wall 
+    wall = Wall(460, 110, 370, 60)
+    wall_list.add(wall)
+
+    # right wall 
+    wall = Wall(775, 130, 25, 600)
+    wall_list.add(wall)
+
+    # top right desk 
+    wall = Wall(470, 265, 60, 40)
+    wall_list.add(wall)
+
+    # top left desk 
+    wall = Wall(290, 265, 60, 40)
+    wall_list.add(wall)
+
+    # bottom right desk 
+    wall = Wall(470, 375, 60, 50)
+    wall_list.add(wall)
+
+    # bottom left desk 
+    wall = Wall(290, 375, 60, 50)
+    wall_list.add(wall)
 
 
+
+    all_sprite_list.add(wall)
+
+
+    
+    miniGame1 = True
+    while miniGame1 == True:
+        pygame.mixer.init()
+        pygame.time.delay(100)
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit()
+
+        keys = pygame.key.get_pressed()
+        mouse1 = pygame.mouse.get_pressed()
+        #pygame.mouse.set_visible()
+
+        if (mouse1 == (1,0,0)):
+            mouseClick()
+
+        window.fill((0,0,0))
+
+
+
+
+                
+
+        pygame.draw.rect(window, (255, 255, 255), (lever1X, leverY, leverWidth, leverHeight))
+        pygame.draw.rect(window, (255, 255, 255), (lever2X, leverY, leverWidth, leverHeight))
+        pygame.draw.rect(window, (255, 255, 255), (lever3X, leverY, leverWidth, leverHeight))
+        pygame.draw.rect(window, (255, 255, 255), (lever4X, leverY, leverWidth, leverHeight))
+
+        pygame.draw.rect(window, (255,0,0), (x,y, width,height))
+        pygame.draw.rect(window, (255, 0, 0), (x+500, y, width, height))
+        pygame.draw.rect(window, (255,0,0), (lineX,lineY,lineWidth,lineHeight))
+        pygame.draw.circle(window, toggle1, (x+ 650, y+ 7), cirRad, 0)
+
+        pygame.draw.rect(window, (0, 255, 0), (x, y+100, width, height))
+        pygame.draw.rect(window, (0, 255, 0), (x + 500, y+100, width, height))
+        pygame.draw.rect(window, (0, 255, 0), (lineX, lineY+100, lineWidth, lineHeight))
+        pygame.draw.circle(window, toggle2, (x + 650, y + 107), cirRad, 0)
+
+        pygame.draw.rect(window, (0, 0, 255), (x, y + 200, width, height))
+        pygame.draw.rect(window, (0, 0, 255), (x + 500, y + 200, width, height))
+        pygame.draw.rect(window, (0, 0, 255), (lineX, lineY+200, lineWidth, lineHeight))
+        pygame.draw.circle(window, toggle3, (x + 650, y + 207), cirRad, 0)
+
+        pygame.draw.rect(window, (255,255,0), (x, y + 300, width, height))
+        pygame.draw.rect(window, (255,255,0), (x + 500, y + 300, width, height))
+        pygame.draw.rect(window, (255, 255, 0), (lineX, lineY+300, lineWidth, lineHeight))
+        pygame.draw.circle(window, toggle4, (x + 650, y + 307), cirRad, 0)
+
+
+        pygame.display.update()
+
+        if (toggle1 == GREEN):
+            if (toggle2 == GREEN):
+                if (toggle3 == GREEN):
+                    if (toggle4 == GREEN):
+                        myfont = pygame.font.SysFont("Arial", 60)
+                        label = myfont.render("You Win!", 1, (255, 255, 0))
+
+                        window.blit(label, (500, 450))
+
+
+
+                        ## add to another level function
+                        miniGame1 = False
+                        running = True
+                        game_sprites.add(security_spots1)
+                        # draw background image - specify image file and rect to load image    
+
+                        
+
+
+                    
+                    
 
 running = True
 # create game loop
 while running:
+
     # check loop is running at set speed
     clock.tick(FPS)
 
@@ -373,11 +630,10 @@ while running:
     # check collisions for game window
     if collisions:
         interaction = "bookshelf"
-        # draw
-        bg_img = pygame.image.load(os.path.join(img_dir, "main_room2.png")).convert()
         # add rect for bg - helps locate background
         window.fill(BLACK)
         # draw background image - specify image file and rect to load image
+        level += 1
         nextLvl()
 
     else:
@@ -392,8 +648,11 @@ while running:
         print('gotcha')
 
     # draw background image - specify image file and rect to load image    
-    window.blit(bg_img, bg_rect)
-        
+    if level == 1:
+        window.blit(bg_img, bg_rect)
+    elif level == 2 :
+        window.blit(bg_img2, bg_rect2)
+
 
 
 
